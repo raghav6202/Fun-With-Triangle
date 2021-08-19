13c45e5d29866d7dcf6db7dcf281e76238a04698
raghav6202 <raghavchandravs@gmail.com>
2021-08-19 10:50:13 +0530

    build:js

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const inputs = document.querySelectorAll(".angle");
+
+const isTriangleBtn = document.querySelector("#check-button");
+
+const outputDisplay = document.querySelector("#output");
+
+
+//there will be various functions created for different operations
+
+function isTriangle ()
+{
+
+
+
+}
+
+isTriangleBtn.addEventListener("click",isTriangle)
