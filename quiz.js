5c0a58058e2507fb100f0076d1f0d3e72b67ddb3
raghav6202 <raghavchandravs@gmail.com>
2021-08-19 23:00:40 +0530

    build:js

--- quiz.js
+++ quiz.js
@@ -1,4 +1,8 @@
 const quizForm = document.querySelector(".quiz-form");
+
 const submitAnswerBtn = document.querySelector("#submit-button");
 
-const outputDisplay = document.querySelector("#score-display")
+const outputDisplay = document.querySelector("#score-display");
+
+
+const answers = ["90°", "right angled"];
